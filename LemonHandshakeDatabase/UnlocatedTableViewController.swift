2016e8541da28520e02bf9fede26a6571e54f7a3
Chrisb616 <cboynton16@gmail.com>
2016-11-21 15:31:47 -0500

    About to import GeoFire

--- LemonHandshakeDatabase/UnlocatedTableViewController.swift
+++ LemonHandshakeDatabase/UnlocatedTableViewController.swift
@@ -21,6 +21,11 @@ class UnlocatedTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
